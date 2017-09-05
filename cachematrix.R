c15d57bd0b8bf56f48e93b456c7d27b03b784ce7
neerjachinda <neerja.chinda@gmail.com>
2017-09-05 16:38:28 -0400

    Updated function to compute the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For running time consuming computations, caching the value makes sense which 
+## can be looked up again when needed rather than computing again
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing following functions to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv<-NULL
+        set<-function(y=matrix()) {
+                # use `<<-` to assign a value to an object in an environment 
+                # different from the current environment.       
+                x<<-y
+                inv<<-NULL
+        }
+        
+        get<- function () {
+                x
+        }
+        
+        setInverse<- function(inv) {
+                inv<<-solve(x)
+        }
+        
+        getInverse<-function() {
+                inv
+        }
+        
+        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setInverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inverse <-x$getInverse()
+        if(!is.null(inverse)) {
+                message("Getting cached data")
+                return(inverse)
+        }
+        matData<-x$get()
+        
+        #Checks if the matrix is invertible
+        if(det(matData)==0) {
+                message("Matrix is not invertible")
+                return(matData)
+        }
+        inverse<-solve(matData)
+        x$setInverse(inverse)
+        inverse
 }
